c1bafe7b573c1cbc19f2b0c4294215e0600533ef
FleecyBear <nikusha.kk1990@gmail.com>
2024-09-22 01:58:20 +0400

    Added details

--- src/Header.js
+++ src/Header.js
@@ -5,14 +5,14 @@ export default function Header() {
   return (
     <header className="Header">
       <div className="Header_Left">
-        <button className="Logo_Button"></button>
-        <button className="Search_Button"></button>
+        <button className="Home_Button"></button>
+        <input type="text" className="Search_Button" placeholder="Search" />
       </div>
       
       <div className="Header_Right">
         <button className="Favorites_Button"></button>
         <button className="Cart_Button"></button>
-        <button className="Login_Button"></button>
+        <button className="Login_Button">Login</button>
       </div>
     </header>
   );
--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './Main.css';
+import panda1 from './panda1.png';
+import panda2 from './panda2.png';
+import panda3 from './panda3.png';
+
 export default function Main() {
   return (
-    <main className='Main'>Main</main>
-  )
+    <main className='Main'>
+      <p className = "Main_Text_First">Buy panda of  your choice!</p>
+      <div className="Main_Images">
+        <img src={panda2} alt="Panda 2" />
+        <img src={panda1} alt="Panda 1" />
+        <img src={panda3} alt="Panda 3" />
+      </div>
+      <p className = "Main_Text_Last">Custom-made pandas from Japan</p>
+    </main>
+  );
 }
