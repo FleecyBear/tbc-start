be27dc164e52422a8ba8537f27868049a13daad0
FleecyBear <nikusha.kk1990@gmail.com>
2024-09-25 17:59:11 +0400

    Updated AddToCartButton
    Changed Addtocartbutton as separate object

---
+++ src/Content/Button/AddToCartButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AddToCartButton.css'
+
+function AddToCartButton({ onClick }) {
+  return (
+    <button className="add-to-cart-button"> Add To Cart </button>
+  );
+}
+
+export default AddToCartButton;
--- src/Content/Products/Products.jsx
+++ src/Content/Products/Products.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Products.css';
-
+import AddToCartButton from '../Button/AddToCartButton';
 export default function Products({ image, description }) {
   return (
     <div className="Main_Grid"> 
       <img src={image} alt="Product" />
       <div className="description">{description}</div>
-      <button>Add To Cart</button>
+      <AddToCartButton/>
     </div>
   );
 }
