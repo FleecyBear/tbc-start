367020c28c65341192922c4fa8fb2f112eef23c9
FleecyBear <nikusha.kk1990@gmail.com>
2024-09-29 19:18:49 +0400

    Added pages
    1. Added /about, /assignment-3 and /contact pages with empty content;
    2. Made Footer and Header appear on all pages without change;
    3. Updated pages' content to match height and width;

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Content/Header/Header';
 import Footer from './Content/Footer/Footer';
 import Main from './Content/Main/Main';
+import About from './Content/About/About';
+import Contact from './Content/Contact/Contact';
+import Assignment3 from './Content/Assignment-3/Assignment3';
+
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <Router basename="/tbc-start">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/assignment-3" element={<Assignment3 />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/Content/Assignment-3/Assignment3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Assignment3.css'
+export default function Assignment3() {
+  return (
+    <div className = "Assignment-3-Main">
+
+    </div>
+  )
+}
