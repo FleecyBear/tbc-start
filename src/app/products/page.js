87463c4af4495a7f4e925a0d3de7eff919183935
andriadze66 <giorgiandriadze814@gmail.com>
2024-10-06 20:05:14 +0400

    Create API for  Product and Blog Pages

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,14 +1,16 @@
 // import Image from 'next/image';
 import "./Products.css";
 import CustomButton from "../button/button.js";
-export default function Products({ image, description, title, buttonText }) {
+export default function Products(props) {
   return (
     <div className="Main_Grid">
       <div className="Title-Image">
-        <img src={image} alt="Product" />
-        <p>{title}</p>
+        <img src={props.image} alt="Product" />
+        <p>{props.title}</p>
+        <p>{props.brand}</p>
       </div>
-      <p className="description">{description}</p>
+      <p className="description">{props.description}</p>
+      <p>{props.price}</p>
       <CustomButton buttonText="Add To Cart" />
     </div>
   );
--- src/app/productsPage/page.js
+++ src/app/productsPage/page.js
@@ -5,28 +5,28 @@ import "./productsPage.css";
 import { useEffect, useState } from "react";
 
 export default function ProductsPage() {
-  const products = [
-    {
-      image: "/images/item1.jpg",
-      description:
-        "Bamboo Tube Panda Doll Plush Toy Cute Holding Bamboo Panda Doll Home Pillow",
-      title: "Panda Toy 1",
-    },
-    {
-      image: "/images/item2.jpg",
-      description:
-        "Bamboo Tube Panda Doll Plush Toy Cute Hiding In Bamboo Tree",
-      title: "Panda Toy 2",
-    },
-    {
-      image: "/images/item3.jpg",
-      description:
-        "Bear Panda Bubu and Dudu Fashion Cartoon Nightlight LED Night Cute Light Lamp",
-      title: "Panda Toy 3",
-    },
-  ];
+  // const products = [
+  //   {
+  //     image: "/images/item1.jpg",
+  //     description:
+  //       "Bamboo Tube Panda Doll Plush Toy Cute Holding Bamboo Panda Doll Home Pillow",
+  //     title: "Panda Toy 1",
+  //   },
+  //   {
+  //     image: "/images/item2.jpg",
+  //     description:
+  //       "Bamboo Tube Panda Doll Plush Toy Cute Hiding In Bamboo Tree",
+  //     title: "Panda Toy 2",
+  //   },
+  //   {
+  //     image: "/images/item3.jpg",
+  //     description:
+  //       "Bear Panda Bubu and Dudu Fashion Cartoon Nightlight LED Night Cute Light Lamp",
+  //     title: "Panda Toy 3",
+  //   },
+  // ];
   
-  const [productList, serProductList] = useState();
+  const [productList, serProductList] = useState([]);
   
   ///immediate invoke function
   useEffect(()=>{
@@ -44,12 +44,14 @@ export default function ProductsPage() {
 },[])
   return (
       <div className="Product_List">
-        {products.map((product, index) => (
+        {productList.map((product, index) => (
           <Products
-            key={index}
-            image={product.image}
+            key={product.id}
+            image={product.images}
             description={product.description}
             title={product.title}
+            price={product.price}   
+            brand={product.brand}
           />
         ))}
       </div>
