6cf614cb61580200bd25db424b6232837e98f383
FleecyBear <nikusha.kk1990@gmail.com>
2024-10-16 05:47:25 +0400

    Refactor: Blogspage/blog-items
    Made it into server component

--- src/app/BlogsPage/blog-items/page.js
+++ src/app/BlogsPage/blog-items/page.js
@@ -1,23 +1,17 @@
-"use client";
 import "./BlogItems.css";
 import CustomButton from "../../button/button.js";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function BlogItems({ blogTitle, blogDescription, id }) {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push(`/BlogsPage/${id}`);
-  };
-
   return (
-    <div className="blogItemsContent" >
+    <div className="blogItemsContent">
       <p className="blogItemsTitle">{blogTitle}</p>
       <p className="blogItemsDescription">{blogDescription}</p>
-      <div onClick={handleClick} style={{ display: 'inline-block' }}>
-        <CustomButton  buttonText="Continue reading..." />
-      </div>
+      <Link href={`/BlogsPage/${id}`} passHref>
+        <div style={{ display: 'inline-block' }}>
+          <CustomButton buttonText="Continue reading..." />
+        </div>
+      </Link>
     </div>
   );
 }
-
