d24d01f1feb1fb28c9e37567f3404ae7d8b3a2e7
FleecyBear <nikusha.kk1990@gmail.com>
2024-10-16 05:45:16 +0400

    Refactor: blogsPage/[id]
    Made it into server component

--- src/app/BlogsPage/[id]/page.js
+++ src/app/BlogsPage/[id]/page.js
@@ -1,36 +1,15 @@
-"use client"
-import { useEffect, useState } from 'react';
+import LikeIcon from '../../svgIcons/likeIcon';
+import DislikeIcon from '../../svgIcons/dislikeIcon';
 import "../detailBlogs.css";
-import LikeIcon from '../../svgIcons/likeIcon'; 
-import DislikeIcon from '../../svgIcons/dislikeIcon'; 
 
-export default function BlogPost({ params }) {
-  const [postData, setPostData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function fetchPost() {
-      try {
-        const res = await fetch(`https://dummyjson.com/posts/${params.id}`);
-        if (!res.ok) {
-          throw new Error("Blog post not found");
-        }
-        const data = await res.json();
-        setPostData(data);
-      } catch (error) {
-        console.error('Failed to fetch blog post:', error);
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    }
-
-    fetchPost();
-  }, [params.id]);
-
-  if (loading) return <p>Loading...</p>;
-  if (error || !postData) return <p>{error || "Blog was not retrieved."}</p>;
+export default async function BlogPost({ params }) {
+  const res = await fetch(`https://dummyjson.com/posts/${params.id}`);
+  
+  if (!res.ok) {
+    throw new Error("Blog post not found");
+  }
+  
+  const postData = await res.json();
 
   return (
     <div className='detail-blog-container'> 
