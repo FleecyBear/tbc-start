567f617c7b38e4d2048b090af4c416111ef7c6d9
FleecyBear <nikusha.kk1990@gmail.com>
2024-10-29 11:56:52 +0400

    Changed main layout
    1. Removed header and footer from main layout;
    2. Changed global.css to fix layout;
    3. Tweaked homepage html and Css.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,8 @@
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
 import "./global.css";
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'; 
 config.autoAddCss = false; 
+
 export const metadata = {
   title: "React App",
   description: "Web site created with Next.js.",
@@ -13,23 +12,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossorigin
-        ></link>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet"
-        ></link>
+        />
       </head>
 
       <body>
         <div className="App">
-          <Header />
           <main className="main">{children}</main>
-          <Footer />
         </div>
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,39 @@
-"use client"
+"use client";
 import Image from "next/image";
 import "./home.css";
 
 export default function Main() {
   return (
     <main className="home_main">
-      <p className="Main_Text_First">Shop Your Favorite Items, All with a Panda Twist!</p>
+      <p className="Main_Text_First">
+        Shop Your Favorite Items, All with a Panda Twist!
+      </p>
       <div className="Main_Images">
         <Image
           src="/images/panda2.png"
           alt="Panda 2"
           width={300}
           height={300}
+          className="panda-image"
         />
         <Image
           src="/images/panda1.png"
           alt="Panda 1"
           width={300}
           height={300}
+          className="panda-image"
         />
         <Image
           src="/images/panda3.png"
           alt="Panda 3"
           width={300}
           height={300}
+          className="panda-image"
         />
       </div>
-      <p className="Main_Text_Last">Everything You Need, Wrapped in Panda Love!</p>
-
-      </main>
+      <p className="Main_Text_Last">
+        Everything You Need, Wrapped in Panda Love!
+      </p>
+    </main>
   );
 }
