aa71d69655bdd96fc49ef3d5a89283072e3e82ed
FleecyBear <nikusha.kk1990@gmail.com>
2024-11-02 16:35:45 +0400

    Added custom blogs
    1. You can now add custom blogs;
    1.1. Custom blogs are saved in local storage;
    2. You can remove blogs;
    3. Fetched blog deletions are not saved upon refresh.

--- src/app/(authPages)/BlogsPage/blog-items/page.js
+++ src/app/(authPages)/BlogsPage/blog-items/page.js
@@ -2,7 +2,7 @@ import "./BlogItems.css";
 import CustomButton from "../../../components/button/button.js";
 import Link from "next/link";
 
-export default function BlogItems({ blogTitle, blogDescription, id }) {
+export default function BlogItems({ blogTitle, blogDescription, id, onDelete }) {
   return (
     <div className="blogItemsContent">
       <p className="blogItemsTitle">{blogTitle}</p>
@@ -12,6 +12,9 @@ export default function BlogItems({ blogTitle, blogDescription, id }) {
           <CustomButton buttonText="Continue reading..." />
         </div>
       </Link>
+      <div onClick={() => onDelete(id)} style={{ display: 'inline-block' }}>
+      <CustomButton buttonText="Delete Blog" />
+      </div>
     </div>
   );
 }
