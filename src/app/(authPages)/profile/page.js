da77700bc45df7bbb0245d99c236805e01e3361e
FleecyBear <nikusha.kk1990@gmail.com>
2024-11-04 13:29:47 +0400

    Updated /profile
    1. Now displaying more user information;
    2. Added simple TailWind styles;
    3. Removed extra import of useState.

--- src/app/(authPages)/profile/page.js
+++ src/app/(authPages)/profile/page.js
@@ -1,30 +1,46 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
-import './Profile.css';
 
 export default function Profile() {
   const { user, error, isLoading } = useUser();
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>{error.message}</div>;
+
+  if (isLoading) return <div className="text-center text-lg">Loading...</div>;
+  if (error) return <div className="text-red-500">{error.message}</div>;
+
+  const getUserDetail = (detail) => {
+    return detail ? detail : 'Not set up'; 
+  };
 
   return (
     user ? (
-      <main className="profile_container">
-        <div>
-          <img src={user.picture} alt={user.name} className="profile_picture" />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
+      <main className="flex flex-col items-center justify-center p-6 text-white ">
+        <div className="flex flex-col items-center mb-4">
+          <img src={user.picture} alt={user.name} className="w-32 h-32 rounded-full border-2 border-white mb-4" />
+          <h2 className="text-2xl font-bold">{getUserDetail(user.nickname)}</h2>
+          <p className="text-lg">{getUserDetail(user.email)}</p>
         </div>
+
+        <div className="bg-purple-600 p-4 rounded-lg w-full max-w-md">
+          <h3 className="text-lg font-semibold">Additional Information</h3>
+          <p><strong>Name:</strong> {getUserDetail(user.name)}</p>
+          <p><strong>Nickname:</strong> {getUserDetail(user.nickname)}</p>
+          <p><strong>Username:</strong> {getUserDetail(user.username)}</p>
+          <p><strong>Email:</strong> {getUserDetail(user.email)}</p>
+          <p><strong>Phone Number:</strong> {getUserDetail(user.phone_number)}</p>
+          <p><strong>Phone Verified:</strong> {user.phone_verified ? 'Yes' : 'No'}</p>
+        </div>
+
         <button 
           onClick={() => {
             window.location.href = '/api/auth/logout';
-          }} >
+          }} 
+          className="mt-4 px-4 py-2 btn-custom">
           Logout
         </button>
       </main>
     ) : (
-      <div>You are not logged in.</div>
+      <div className="text-center text-lg">You are not logged in.</div>
     )
   );
 }
