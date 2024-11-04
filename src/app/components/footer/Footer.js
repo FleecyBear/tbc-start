663080e3be1deeb5791374d4292f093f9df405bd
FleecyBear <nikusha.kk1990@gmail.com>
2024-11-04 16:09:10 +0400

    Update Footer.js
    1. Removed blogs and asignment 3 from footer;
    2. Changed font size to smaller.

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -4,16 +4,10 @@ export default function Footer() {
   return (
     <footer className="relative w-full h-20 bg-slate-200
       flex justify-between items-center mt-auto z-50 shadow-md 
-      dark:bg-gray-900 border-t-4 border-blue-500 px-32">
+      dark:bg-gray-900 border-t-4 border-blue-500 px-32 text-sm">
       <div className="flex gap-12 items-center">
         <Link href="/contact" passHref>
           <button className="btn-custom">Contact</button>
-        </Link>
-        <Link href="/assignment-3" passHref>
-          <button className="btn-custom">Assignment 3</button>
-        </Link>
-        <Link href="/BlogsPage" passHref>
-          <button className="btn-custom">Blog</button>
         </Link>
         <Link href="/about" passHref>
           <button className="btn-custom">About</button>
