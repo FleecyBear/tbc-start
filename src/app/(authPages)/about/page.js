dd742e3768c826ca6a2ccbfb6fb1a9ed7212b68d
FleecyBear <nikusha.kk1990@gmail.com>
2024-11-05 19:09:09 +0400

    Updated styles
    1. Updated btn-custom background color to match header/footer color;
    2. Updated /about page:
    2.1. removed about.css;
    2.2 replaced old css with TailWind styles.
    2.3. added dark mode styles for /about page

--- src/app/(authPages)/about/page.js
+++ src/app/(authPages)/about/page.js
@@ -1,22 +1,33 @@
-import "./About.css";
-
 export default function About() {
   return (
     <>
-      <h1 className="about-title">About TBC Project</h1>
-
-      <section className="small-sections">
-        <h2 className="small-title">Welcome to the TBC Project!</h2>
-        <p className="small-text">
+      <h1 className="text-3xl font-bold text-center mt-8 mb-6 text-gray-800
+      dark:text-gray-300">
+        About TBC Project
+      </h1>
+      
+      <section className="bg-gray-200 p-6 rounded-lg shadow-md mb-6 mx-4
+       dark:bg-gray-900">
+        <h2 className="text-xl font-semibold mb-2 text-blue-700 text-center
+        dark:text-blue-400">
+          Welcome to the TBC Project!
+        </h2>
+        <p className="text-gray-700 text-base leading-relaxed text-center
+        dark:text-gray-200">
           Our mission is to spread joy and positivity through our adorable panda
           toys. Inspired by the gentle nature of these beloved animals, we aim
           to bring a smile to children and adults alike.
         </p>
       </section>
 
-      <section className="small-sections">
-        <h2 className="small-title">Our Panda Toys</h2>
-        <p className="small-text">
+      <section className="bg-gray-200 p-6 rounded-lg shadow-md mb-6 mx-4
+      dark:bg-gray-900">
+        <h2 className="text-xl font-semibold mb-2 text-blue-700 text-center
+        dark:text-blue-400">
+          Our Panda Toys
+        </h2>
+        <p className="text-gray-700 text-base leading-relaxed text-center
+        dark:text-gray-200">
           Our collection features a range of panda toys, from cuddly plush
           companions to fun educational figures. Each toy is crafted with care
           using eco-friendly materials, ensuring that our products are not only
@@ -24,9 +35,14 @@ export default function About() {
         </p>
       </section>
 
-      <section className="small-sections">
-        <h2 className="small-title">Why Pandas?</h2>
-        <p className="small-text">
+      <section className="bg-gray-200 p-6 rounded-lg shadow-md mb-6 mx-4
+      dark:bg-gray-900">
+        <h2 className="text-xl font-semibold mb-2 text-blue-700 text-center
+        dark:text-blue-400">
+          Why Pandas?
+        </h2>
+        <p className="text-gray-700 text-base leading-relaxed text-center
+        dark:text-gray-200">
           Pandas are not just cute; they symbolize peace and conservation. By
           choosing our panda toys, you are not only getting a delightful
           companion but also supporting efforts to protect these amazing
