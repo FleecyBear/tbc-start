60eccfff836fc6c74c2a7b8e01e73746cd96d3fd
FleecyBear <nikusha.kk1990@gmail.com>
2024-11-08 22:33:35 +0400

    Update languageSelector.js
    Modified style

--- src/app/utils/languageSelector.js
+++ src/app/utils/languageSelector.js
@@ -25,10 +25,12 @@ export default function LanguageSelector() {
     <select
       value={lang}
       onChange={handleLanguageChange}
-      className="px-4 py-2 border rounded-md bg-white text-gray-700 dark:bg-gray-800 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="px-4 py-2 border rounded-md border-blue-500 bg-transparent text-black
+       dark:text-white  
+      focus:outline-none focus:ring-2 focus:ring-blue-500 "
     >
-      <option value="ge">GE</option>
-      <option value="en">EN</option>
+      <option value="ge" className=" bg-slate-200 dark:bg-gray-800">GE</option>
+      <option value="en" className=" bg-slate-200 dark:bg-gray-800">EN</option>
     </select>
   );
 }
