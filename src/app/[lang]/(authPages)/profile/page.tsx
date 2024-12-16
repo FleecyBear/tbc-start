bf86e84b48194b231b75b97c10632a493953bb20
FleecyBear <nikusha.kk1990@gmail.com>
2024-12-16 17:59:32 +0400

    Build optimization

--- src/app/[lang]/(authPages)/profile/page.tsx
+++ src/app/[lang]/(authPages)/profile/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useEffect } from 'react';
 import { useUser } from '@auth0/nextjs-auth0/client';
 
 interface UserType {
--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,43 +1,28 @@
 'use client';
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import ThemeToggle from '../../utils/themeToggle';
 import LanguageSelector from '../../utils/languageSelector';
 import useTranslation from '../../utils/useTranslation';
 
-interface Translations {
-  home: string;
-  products: string;
-  blogs: string;
-  profile: string;
-  logout: string;
-}
-
 export default function Header() {
   const currentLang: string = typeof window !== 'undefined' ? localStorage.getItem('lang') || 'en' : 'en';
   const translations = useTranslation(currentLang);
 
-  const isLoading = Object.values(translations).some((value) => !value);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <header className="fixed top-0 left-0 w-full h-20 bg-white flex justify-between items-center z-50 shadow-md dark:bg-gray-900 border-b-4 border-blue-500 px-32">
       <div className="flex gap-12 items-center">
-        <Link href={`/${currentLang}/home`} passHref>
+        <Link href={`/${currentLang}/home`}>
           <button className="btn-custom">{translations.home}</button>
         </Link>
-        <Link href={`/${currentLang}/productsPage`} passHref>
+        <Link href={`/${currentLang}/productsPage`}>
           <button className="btn-custom">{translations.products}</button>
         </Link>
-        <Link href={`/${currentLang}/BlogsPage`} passHref>
+        <Link href={`/${currentLang}/BlogsPage`}>
           <button className="btn-custom">{translations.blogs}</button>
         </Link>
       </div>
       <div className="flex gap-12 items-center pl-12">
-        <Link href={`/${currentLang}/profile`} passHref>
+        <Link href={`/${currentLang}/profile`}>
           <button className="btn-custom">{translations.profile}</button>
         </Link>
         <button
