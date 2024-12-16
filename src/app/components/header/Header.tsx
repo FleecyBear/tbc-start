670ab1baf2aed6c40e8f7cc90cb7628249deccb9
FleecyBear <nikusha.kk1990@gmail.com>
2024-12-16 15:55:22 +0400

    Refactor: themeToggle

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import ThemeToggle from '../../utils/themeToggle.js';
+import ThemeToggle from '../../utils/themeToggle';
 import LanguageSelector from '../../utils/languageSelector';
 import useTranslation from '../../utils/useTranslation';
 
