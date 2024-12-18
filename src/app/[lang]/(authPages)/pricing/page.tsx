8e3939356ce8d4d94c1f5e6d0718ef7e6577956d
FleecyBear <nikusha.kk1990@gmail.com>
2024-12-18 12:09:56 +0400

    Installed: Stripe
    Added donate-with-checkout option

---
+++ src/app/[lang]/(authPages)/pricing/page.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+
+
+export default function Pricing() {
+  return (
+    <ul className="card-list">
+      <li>
+        <Link
+          href="/donate-with-embedded-checkout"
+          className="card checkout-style-background"
+        >
+          <h2 className="bottom">Donate with embedded Checkout</h2>
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/donate-with-checkout"
+          className="card checkout-style-background"
+        >
+          <h2 className="bottom">Donate with hosted Checkout</h2>
+        </Link>
+      </li>
+      <li>
+        <Link
+          href="/donate-with-elements"
+          className="card elements-style-background"
+        >
+          <h2 className="bottom">Donate with Elements</h2>
+        </Link>
+      </li>
+    </ul>
+  );
+}
