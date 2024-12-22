23afcb014a63a01c32b48a0c00126011c27d218d
FleecyBear <nikusha.kk1990@gmail.com>
2024-12-22 14:10:02 +0400

    Refactor: auth layout
    No longer checking for auth0 user

--- src/app/[lang]/(authPages)/layout.tsx
+++ src/app/[lang]/(authPages)/layout.tsx
@@ -1,7 +1,4 @@
 'use client';
-import { useEffect } from 'react';
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { useRouter } from 'next/navigation';
 import Header from '../../components/header/Header';
 import Footer from '../../components/footer/Footer';
 import Image from 'next/image';
@@ -11,22 +8,6 @@ interface AuthLayoutProps {
 }
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
-  const { user, isLoading } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isLoading && !user) {
-      router.push('/api/auth/login');
-    }
-  }, [isLoading, user, router]);
-
-  if (isLoading || (!user && typeof window !== 'undefined')) {
-    return (
-      <div className="flex items-center justify-center min-h-screen bg-violet-700">
-        <div className="text-white text-lg">Loading...</div>
-      </div>
-    );
-  }
 
   return (
     <div className="flex flex-col min-h-screen relative">
