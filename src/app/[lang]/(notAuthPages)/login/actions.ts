f8be0af7116374aca4de7bd5d27ee584074e5f69
FleecyBear <nikusha.kk1990@gmail.com>
2025-01-14 03:31:12 +0400

    Feat: signUp
    creates table record in profiles during signup

--- src/app/[lang]/(notAuthPages)/login/actions.ts
+++ src/app/[lang]/(notAuthPages)/login/actions.ts
@@ -1,4 +1,4 @@
-// actions.ts
+
 'use server'
 
 import { revalidatePath } from 'next/cache'
@@ -29,10 +29,35 @@ export async function signup(formData: FormData, redirectTo: string = '/') {
     password: formData.get('password') as string,
   }
 
-  const { error } = await supabase.auth.signUp(data)
+  const { data: authData, error } = await supabase.auth.signUp(data)
 
   if (error) {
+    console.error('Signup error:', error)
     redirect('/error')
+  }
+
+  // Now access the user from authData
+  const user = authData.user
+
+  // Check if user exists
+  if (user) {
+    const { email, id } = user // `id` is the UUID of the user
+    const profileData = {
+      user_id: id,      // This is the UUID from authentication
+      email: email,
+      nickname: email,  // Set the nickname to the email by default
+      coins: 100,       // Default coins
+    }
+
+    // Insert the new profile into the profiles table
+    const { error: profileError } = await supabase
+      .from('profiles')
+      .insert([profileData])
+
+    if (profileError) {
+      console.error('Error creating profile:', profileError)
+      redirect('/error')
+    }
   }
 
   revalidatePath('/', 'layout')
