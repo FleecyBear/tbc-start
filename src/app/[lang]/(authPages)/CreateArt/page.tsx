8989ba3f6b615d59c74f622ace092ba73c425ed1
FleecyBear <nikusha.kk1990@gmail.com>
2025-01-14 21:16:44 +0400

    Styles: Create Art

--- src/app/[lang]/(authPages)/CreateArt/page.tsx
+++ src/app/[lang]/(authPages)/CreateArt/page.tsx
@@ -156,19 +156,31 @@ export default function CreateArtPage() {
         <h1 className="text-2xl mb-4 h2-1">Create Art</h1>
 
         {!isGridVisible && (
-          <div className="mb-4">
-            <p>Size of grid?</p>
-            <button onClick={() => handleGridSizeChange(5)} className="mx-2">
+        <div className="mb-4 flex flex-col items-center">
+          <p className="text-lg font-medium mb-4 p-1">Choose size:</p>
+          <div className="flex gap-4">
+            <button
+              onClick={() => handleGridSizeChange(5)}
+              className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none transition-all duration-300"
+            >
               5x5
             </button>
-            <button onClick={() => handleGridSizeChange(10)} className="mx-2">
+            <button
+              onClick={() => handleGridSizeChange(10)}
+              className="px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 focus:outline-none transition-all duration-300"
+            >
               10x10
             </button>
-            <button onClick={() => handleGridSizeChange(20)} className="mx-2">
+            <button
+              onClick={() => handleGridSizeChange(20)}
+              className="px-4 py-2 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600 focus:outline-none transition-all duration-300"
+            >
               20x20
             </button>
           </div>
-        )}
+        </div>
+      )}
+
 
         {isGridVisible && (
           <>
